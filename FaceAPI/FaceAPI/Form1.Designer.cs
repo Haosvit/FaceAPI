17ba03aba5a4634f1d0521b2a57282fabd343f9f
Hao Svit <haonguyen.svit@gmail.com>
2016-10-20 14:11:16 +0700

    + Added project

---
+++ FaceAPI/FaceAPI/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace FaceAPI
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this._btnDetect = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this._btnBrowse = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this._lblGender = new System.Windows.Forms.Label();
+			this._lblAge = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// _btnDetect
+			// 
+			this._btnDetect.Location = new System.Drawing.Point(12, 41);
+			this._btnDetect.Name = "_btnDetect";
+			this._btnDetect.Size = new System.Drawing.Size(116, 23);
+			this._btnDetect.TabIndex = 0;
+			this._btnDetect.Text = "Detect Face(s)";
+			this._btnDetect.UseVisualStyleBackColor = true;
+			this._btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// _btnBrowse
+			// 
+			this._btnBrowse.Location = new System.Drawing.Point(12, 12);
+			this._btnBrowse.Name = "_btnBrowse";
+			this._btnBrowse.Size = new System.Drawing.Size(75, 23);
+			this._btnBrowse.TabIndex = 1;
+			this._btnBrowse.Text = "Browse";
+			this._btnBrowse.UseVisualStyleBackColor = true;
+			this._btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(105, 17);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(0, 13);
+			this.label1.TabIndex = 2;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(147, 46);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(45, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Gender:";
+			// 
+			// _lblGender
+			// 
+			this._lblGender.AutoSize = true;
+			this._lblGender.Location = new System.Drawing.Point(203, 46);
+			this._lblGender.Name = "_lblGender";
+			this._lblGender.Size = new System.Drawing.Size(10, 13);
+			this._lblGender.TabIndex = 4;
+			this._lblGender.Text = "-";
+			// 
+			// _lblAge
+			// 
+			this._lblAge.AutoSize = true;
+			this._lblAge.Location = new System.Drawing.Point(299, 46);
+			this._lblAge.Name = "_lblAge";
+			this._lblAge.Size = new System.Drawing.Size(10, 13);
+			this._lblAge.TabIndex = 5;
+			this._lblAge.Text = "-";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(262, 46);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(29, 13);
+			this.label5.TabIndex = 6;
+			this.label5.Text = "Age:";
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 81);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.panel1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+			this.splitContainer1.Size = new System.Drawing.Size(730, 631);
+			this.splitContainer1.SplitterDistance = 315;
+			this.splitContainer1.TabIndex = 7;
+			// 
+			// panel1
+			// 
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(730, 315);
+			this.panel1.TabIndex = 4;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(730, 312);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(730, 712);
+			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this._lblAge);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this._lblGender);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this._btnBrowse);
+			this.Controls.Add(this._btnDetect);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Demo Microsoft Face API";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button _btnDetect;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button _btnBrowse;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label _lblGender;
+		private System.Windows.Forms.Label _lblAge;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.ComponentModel.BackgroundWorker backgroundWorker1;
+	}
+}
+
---
+++ FaceAPI/FaceAPI/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace FaceAPI
+{
+	public partial class Form1 : Form
+	{
+		private string _imgPath;
+		private Graphics _graphic;
+		private Pen _pen;
+		private List<RootObject> _faceInfoList;
+		private int _circleSize;
+		private string _returnedJson;
+		public Form1()
+		{
+			InitializeComponent();
+			_graphic = panel1.CreateGraphics();
+			_graphic.Clear(Color.White);
+			_pen = new Pen(new SolidBrush(Color.OrangeRed));
+
+			backgroundWorker1.DoWork += OnSendRequest;
+			backgroundWorker1.RunWorkerCompleted += OnResultReceived;
+		}
+
+		private void btnDetect_Click(object sender, EventArgs e)
+		{
+			backgroundWorker1.RunWorkerAsync();
+		}
+
+		public string getJSON()
+		{
+			try
+			{
+				var request =
+					(HttpWebRequest) WebRequest.Create("https://api.projectoxford.ai/face/v1.0/detect?" +
+					                                   "returnFaceId=true" +
+					                                   "&returnFaceLandmarks=true" +
+													   "&returnFaceAttributes=age,gender,smile");
+				
+				//var postData = "{\"url\":\"http://nguoinoitieng.vn/images/item/tieu-su-ca-si-my-tam.JPG\"}";
+
+				var imgBinary = File.ReadAllBytes(_imgPath);
+
+				request.Method = "POST";
+				request.ContentType = "application/octet-stream";
+				request.ContentLength = imgBinary.Length;
+				request.Host = "api.projectoxford.ai";
+				request.Headers.Add("Ocp-Apim-Subscription-Key", "1c056c36ece84f14a0619803ee4f0ceb");
+
+				using (var stream = request.GetRequestStream())
+				{
+					stream.Write(imgBinary, 0, imgBinary.Length);
+				}
+
+				var response = (HttpWebResponse) request.GetResponse();
+
+				var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+				return responseString;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			} 
+
+			return "";
+		}
+
+		private void btnBrowse_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				_imgPath = openFileDialog1.FileName;
+				label1.Text = _imgPath;
+				DrawImageOnPanel();
+			}
+		}
+
+		private void UpdateFaceInfo()
+		{
+			if (_faceInfoList.Count > 0)
+			{
+				_lblGender.Text = _faceInfoList[0].faceAttributes.gender;
+				_lblAge.Text = _faceInfoList[0].faceAttributes.age.ToString();
+			}
+		}
+
+		private void DrawImageOnPanel()
+		{
+			var image = Image.FromFile(_imgPath);
+			_graphic.Clear(Color.White);
+			_graphic.DrawImage(image, 0, 0, image.Width, image.Height);
+			richTextBox1.Clear();
+		}
+
+		private void DrawFacePointsOnPanel()
+		{
+			foreach (RootObject face in _faceInfoList)
+			{
+				var faceRect = new Rectangle(face.faceRectangle.left, face.faceRectangle.top, face.faceRectangle.width, face.faceRectangle.height);
+				_circleSize = faceRect.Width/20;
+				var leftPupil = new Point((int)face.faceLandmarks.pupilLeft.x, (int)face.faceLandmarks.pupilLeft.y);
+				var rightPupil = new Point((int)face.faceLandmarks.pupilRight.x, (int)face.faceLandmarks.pupilRight.y);
+				var leftMouth = new Point((int)face.faceLandmarks.mouthLeft.x, (int)face.faceLandmarks.mouthLeft.y);
+				var rightMouth = new Point((int)face.faceLandmarks.mouthRight.x, (int)face.faceLandmarks.mouthRight.y);
+				var noseRoot = new Point((int)face.faceLandmarks.noseTip.x, (int)face.faceLandmarks.noseTip.y);
+				_graphic.DrawRectangle(_pen, faceRect);
+				_graphic.DrawEllipse(_pen, new Rectangle(leftPupil, new Size(_circleSize, _circleSize)));
+				_graphic.DrawEllipse(_pen, new Rectangle(rightPupil, new Size(_circleSize, _circleSize)));
+				_graphic.DrawEllipse(_pen, new Rectangle(leftMouth, new Size(_circleSize, _circleSize)));
+				_graphic.DrawEllipse(_pen, new Rectangle(rightMouth, new Size(_circleSize, _circleSize)));
+				_graphic.DrawEllipse(_pen, new Rectangle(noseRoot, new Size(_circleSize, _circleSize)));
+			}
+		}
+		private void Form1_SizeChanged(object sender, EventArgs e)
+		{
+			_graphic = panel1.CreateGraphics();
+			_graphic.Clear(Color.White);
+		}
+
+
+		private void OnSendRequest(object sender, System.ComponentModel.DoWorkEventArgs e)
+		{
+			_returnedJson = getJSON();
+		}
+
+		private void OnResultReceived(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+		{
+			richTextBox1.Text = _returnedJson;
+			_faceInfoList = new List<RootObject>();
+			_faceInfoList = JsonConvert.DeserializeObject<List<RootObject>>(_returnedJson);
+			DrawFacePointsOnPanel();
+			UpdateFaceInfo();
+		}
+
+	}
+}
